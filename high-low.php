445c7d4afc7068a74789801922a6e4f714fd337f
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-07-19 14:29:46 -0500

    Added a guess counter

--- high-low.php
+++ high-low.php
@@ -1,7 +1,7 @@
 <?php
 
 $randomNumber = rand(1, 100);
-echo "{$randomNumber}\n";
+// echo "{$randomNumber}\n";
 
 //Write the output
 fwrite(STDOUT, 'Guess? ');
@@ -9,15 +9,23 @@ fwrite(STDOUT, 'Guess? ');
 //Get the input from the user
 
 do {
-	$userGuess = fgets(STDIN);
-	fwrite(STDOUT, 'Guess? ');
+	$userGuess = trim(fgets(STDIN));
+	$guessNumber = 0;
+	if (! is_numeric($userGuess)) {
+		echo "Hey! That's not a number!" . PHP_EOL;
+		continue;
+	}
 	if ($userGuess < $randomNumber) {
 		echo "HIGHER" . PHP_EOL;
 	} else if ($userGuess > $randomNumber) {
 		echo "LOWER" . PHP_EOL;
 	} else if ($userGuess == $randomNumber) {
+		$guessNumber++;
 		echo "GOOD GUESS!";
+		echo "You guessed {$guessNumber} times!";
 	}
+	$guessNumber++;
+	fwrite(STDOUT, 'Guess? ');
 } while ($userGuess !== $randomNumber);
 
 
